9c9ae2a9b3c8560ef5e4c06490a433743fea2661
Fernando Vega <pytness@gmail.com>
2022-10-24 11:16:48 +0200

    Remove alt-enter keybind for fullscreen

--- config.def.h
+++ config.def.h
@@ -429,7 +429,7 @@ static Shortcut shortcuts[] = {
 #endif  // VIM_BROWSE_PATCH
 #if FULLSCREEN_PATCH
     { XK_NO_MOD, XK_F11, fullscreen, { .i = 0 } },
-    { MODKEY, XK_Return, fullscreen, { .i = 0 } },
+ // { MODKEY, XK_Return, fullscreen, { .i = 0 } },
 #endif  // FULLSCREEN_PATCH
 };
 
